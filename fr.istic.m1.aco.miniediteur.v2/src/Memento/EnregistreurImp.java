1f9ebdcd8b19b98eb47c5e607656b915c480dd52
jbbor <jbbordier@hotmail.fr>
2017-11-13 14:35:11 +0100

    Classe enregistreur

--- fr.istic.m1.aco.miniediteur.v2/src/Memento/EnregistreurImp.java
+++ fr.istic.m1.aco.miniediteur.v2/src/Memento/EnregistreurImp.java
@@ -6,18 +6,30 @@ import Command.*;
 public class EnregistreurImp {
 	private HashMap<Memento,Command> hm ;
 	private boolean isrecording ;
+	private boolean isreplaying;
 	public EnregistreurImp() {
 		hm=new HashMap<Memento,Command>();
 		isrecording = false;
+		isreplaying=false;
 	}
 	public boolean getRecording(){
 		return isrecording;
 	}
+	public boolean getReplaying(){
+		return isreplaying;
+	}
 	public void execute() {
-		
+		for (Memento key   : hm.keySet()) {
+		     hm.get(key).execute();  //get() is less efficient 
+		}    
+	}
+	public HashMap<Memento,Command> getMap (){
+		return this.hm;
 	}
 	public void start() {
+		if(!isreplaying) {
 		isrecording=true;
+		}
 	}
 	public void stop() {
 		isrecording=false;
