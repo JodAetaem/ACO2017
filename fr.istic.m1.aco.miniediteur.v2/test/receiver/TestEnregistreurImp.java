4324fde0d82f844c780b34bc49c45150a33b3656
Jod Aetaem <Jod Aetaem@LAPTOP-R4NU3VD9.home>
2017-11-30 00:16:07 +0100

    Suppression de l'attribut "moteur" et IHM non utilisé (directement init dans l'éditor)

--- fr.istic.m1.aco.miniediteur.v2/test/receiver/TestEnregistreurImp.java
+++ fr.istic.m1.aco.miniediteur.v2/test/receiver/TestEnregistreurImp.java
@@ -14,21 +14,14 @@ import Memento.EnregistreurImp;
 import Memento.Memento;
 import Memento.MementoCopier;
 import moteur.Editeur;
-import moteur.IHM;
-import moteur.Moteur;
-import moteur.MoteurImp;
 
 public class TestEnregistreurImp {
 	
-	private Moteur lemoteur;
-	private IHM interfaces;
 	private Enregistreur recordeur;
 	private Editeur editor;
 
 	@Before
 	public void setUp() throws Exception {
-		lemoteur = new MoteurImp("Bonjour le Monde");
-		interfaces = new IHM();
 		recordeur = new EnregistreurImp ();
 		editor = new Editeur();
 		
