8e967caefed143e63398ca0917b2d409c07e95b2
Jod Aetaem <Jod Aetaem@LAPTOP-R4NU3VD9>
2017-12-06 20:44:26 +0100

    Fin des test et ajout commentaires

--- fr.istic.m1.aco.miniediteur.v3/fr.istic.m1.aco.miniediteur.v3/test/receiver/UndoRedoTest.java
+++ fr.istic.m1.aco.miniediteur.v3/fr.istic.m1.aco.miniediteur.v3/test/receiver/UndoRedoTest.java
@@ -30,8 +30,6 @@ public class UndoRedoTest {
 		editor = new Editeur();
 		undoredo = new UndoRedo(editor.getMoteur());
 		
-		Stack<State> redoCompare = new Stack<State>();
-		
 		editor.getIHM().init("#couper", editor.getCouper());
 		editor.getIHM().init("#coller", editor.getColler());
 		editor.getIHM().init("#copier", editor.getCopier());
@@ -52,6 +50,10 @@ public class UndoRedoTest {
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * Verifie que lors d'une execution du commande changeant l'état du moteur, un nouveau
+	 * state apparait dans le undoredo.undo
+	 */
 	@Test
 	public final void testAddPrevious() { // TODO
 		int tailleavant = editor.getUndoredo().getUndo().size();
@@ -71,30 +73,54 @@ public class UndoRedoTest {
 		
 	}
 
+	/**
+	 * Après un Undo, le state du moteur redo est bien enregistré dans undoredo.redo, mais supprimé après
+	 * un appel d'une nouvelle commande importante
+	 */
 	@Test
 	public final void testAddNext() {
 		
 		editor.getIHM().getMap().get("#couper").execute();
 		int tailleavant = editor.getUndoredo().getRedo().size();
-		editor.getIHM().getMap().get("#undo").execute();
+		editor.getIHM().getMap().get("#undo").execute();	// rajoute un state dans undo
 		int tailleapres =  editor.getUndoredo().getRedo().size();
 		assertTrue(tailleavant<tailleapres);
-		editor.getIHM().getMap().get("#couper").execute();
+		editor.getIHM().getMap().get("#couper").execute(); // exectuer une fonctionn vide le redo
 		tailleavant = editor.getUndoredo().getRedo().size();
 		assertTrue(tailleavant<tailleapres);
 		
 	}
 
+	/**
+	 * Test l'execution du TheUndo (= #undo dans notre editeur de texte)
+	 */
 	@Test
 	public final void testTheUndo() {
-		fail("Not yet implemented"); // TODO
+		editor.getIHM().getMap().get("#couper").execute(); //nouveau state dans undoredo
+		int tailleavant = editor.getUndoredo().getUndo().size();
+		editor.getUndoredo().theUndo(editor.getMoteur()); //enlève un state de undoredo et change le moteur
+		int tailleapres =  editor.getUndoredo().getUndo().size();
+		assertTrue(tailleavant>tailleapres);
 	}
 
+	/**
+	 * Test l'execution du TheRedo (= #redo dans notre editeur de texte)
+	 */
 	@Test
 	public final void testTheRedo() {
-		fail("Not yet implemented"); // TODO
+		editor.getIHM().getMap().get("#couper").execute();
+		editor.getIHM().getMap().get("#undo").execute(); // rajoute un state Redo
+		int tailleavant = editor.getUndoredo().getRedo().size();
+		editor.getUndoredo().theRedo(editor.getMoteur()); //-1 state dans Redo
+		int tailleapres =  editor.getUndoredo().getRedo().size();
+		assertTrue(tailleavant>tailleapres);
+		
+		
 	}
 
+	/**
+	 * test que l'on vide la Stack<StateImp> redo dans undoredo
+	 */
 	@Test
 	public final void testNoMoreRedo() {
 		editor.getIHM().getMap().get("#couper").execute();
